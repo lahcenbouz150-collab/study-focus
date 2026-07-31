6089845010bc501783ceb508506512063770a2dc
lahcenbouz150-collab <lahcenbouz150@gmail.com>
2026-07-31 21:17:17 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -576,3 +576,34 @@ closeBtn.onclick = () => {
     modal.classList.add("hidden");
 
 };
+// Theme Switch
+
+const themeBtn = document.getElementById("themeBtn");
+
+
+if(themeBtn){
+
+    themeBtn.onclick = () => {
+
+        document.body.classList.toggle("light");
+
+
+        localStorage.setItem(
+            "theme",
+            document.body.classList.contains("light")
+            ? "light"
+            : "dark"
+        );
+
+    };
+
+}
+
+
+// Load Theme
+
+if(localStorage.getItem("theme") === "light"){
+
+    document.body.classList.add("light");
+
+}
